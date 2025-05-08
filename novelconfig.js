585be07e0f7eb0c3e96285b9d27d15ecfdd3b2ad
JoaoCussolim <joaovccussolim@gmail.com>
2025-05-08 15:12:47 -0300

    Adaptabilidade
    Aumento da adaptabilidade com paragrafos longos por meio da separação silábica

--- novelconfig.js
+++ novelconfig.js
@@ -3,8 +3,7 @@ const novelMainTitleEl = document.getElementById('novel-main-title-index');
 const novelTitle = "Conquistas do Chico";
 
 const chapters = [
-    { file: 'chapters/ch0.txt', title: 'Capítulo 0', subtitle: 'Prólogo' },
-    { file: 'chapters/ch1.txt', title: 'Capítulo 1', subtitle: 'Parâmetros' },
+    { file: 'chapters/ch1.txt', title: 'Capítulo 1', subtitle: 'Prólogo' },
 ];
 
 novelMainTitleEl.textContent = novelTitle;
