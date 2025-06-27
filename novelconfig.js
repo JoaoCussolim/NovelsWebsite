de95cb197f452f76a24dafef5c377a55dd1888b9
JoaoCussolim <joaovccussolim@gmail.com>
2025-06-27 00:23:07 -0300

    Cap2 e Cap3

--- novelconfig.js
+++ novelconfig.js
@@ -5,6 +5,7 @@ const novelTitle = "Conquistas do Chico";
 const chapters = [
     { file: 'chapters/ch1.txt', title: 'Capítulo 1', subtitle: 'Prólogo' },
     { file: 'chapters/ch2.txt', title: 'Capítulo 2', subtitle: 'Professor Chico' },
+    { file: 'chapters/ch3.txt', title: 'Capítulo 3', subtitle: 'Projeto Gênese' },
 ];
 
 novelMainTitleEl.textContent = novelTitle;
