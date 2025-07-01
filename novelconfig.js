c165148378fef932b9bf3532743f8fe0e81285fa
JoaoCussolim <joaovccussolim@gmail.com>
2025-07-01 04:11:31 -0300

    4 e 5

--- novelconfig.js
+++ novelconfig.js
@@ -6,6 +6,8 @@ const chapters = [
     { file: 'chapters/ch1.txt', title: 'Capítulo 1', subtitle: 'Prólogo' },
     { file: 'chapters/ch2.txt', title: 'Capítulo 2', subtitle: 'Professor Chico' },
     { file: 'chapters/ch3.txt', title: 'Capítulo 3', subtitle: 'Projeto Gênese' },
+    { file: 'chapters/ch4.txt', title: 'Capítulo 4', subtitle: 'Carroceiro Barnabé' },
+    { file: 'chapters/ch5.txt', title: 'Capítulo 5', subtitle: 'Ponente' },
 ];
 
 novelMainTitleEl.textContent = novelTitle;
